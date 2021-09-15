383970fc7eff7cfe3c3e95ff141a629741adb4de
Alan Vivona <alanvivona@github.com>
2021-09-15 22:56:00 +0100

    goroutines

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"sync"
 )
 
 var ErrStatusNotSuccess = errors.New("Response status code != 200")
@@ -13,9 +14,12 @@ type Fetcher struct {
 	client   http.Client
 	uniq     map[string]bool
 	maxDepth int
+	mutex    *sync.Mutex
 }
 
 func (f *Fetcher) isUniq(url string) bool {
+	f.mutex.Lock()
+	defer f.mutex.Unlock()
 	exists := f.uniq[url]
 	if !exists {
 		f.uniq[url] = true
@@ -27,6 +31,7 @@ func NewFetcher() *Fetcher {
 	return &Fetcher{
 		client: http.Client{},
 		uniq:   map[string]bool{},
+		mutex:  &sync.Mutex{},
 	}
 }
 
--- main.go
+++ main.go
@@ -4,26 +4,56 @@ import (
 	"scrapper/fetcher"
 	"scrapper/parser"
 	"scrapper/utils"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 )
 
+type Scrapper struct {
+	fetcher  *fetcher.Fetcher
+	wg       *sync.WaitGroup
+	maxDepth int
+}
+
+func NewScrapper(maxDepth int) *Scrapper {
+	return &Scrapper{
+		fetcher:  fetcher.NewFetcher(),
+		wg:       &sync.WaitGroup{},
+		maxDepth: maxDepth,
+	}
+}
+
+func (s *Scrapper) Run(rootTarget *parser.Target) {
+	s.run(rootTarget)
+	s.wg.Wait()
+}
+
+func (s *Scrapper) run(target *parser.Target) error {
+	log.Info("Fetching ", target.String())
+
+	if target.Depth > s.maxDepth {
+		log.Info("Skipping ", target.String())
+		return nil
+	}
+
+	content, err := s.fetcher.Fetch(target.URL)
+	if err != nil {
+		return err
+	}
+
+	s.wg.Add(1)
+	go func(t *parser.Target) {
+		defer s.wg.Done()
+		for _, newTarget := range t.ParseLinks(content) {
+			s.run(&newTarget)
+		}
+	}(target)
+
+	return nil
+}
+
 func main() {
 	rootURL, maxDepth := utils.ParseEnv()
-	fetcher := fetcher.NewFetcher()
-	targets := []parser.Target{{*rootURL, 0}}
-	for i := 0; i < len(targets); i++ {
-		target := targets[i]
-		if target.Depth > maxDepth {
-			log.Info("Skipping ", target.String())
-			continue
-		}
-		log.Info("Fetching ", target.String())
-		content, err := fetcher.Fetch(target.URL)
-		if err != nil {
-			continue
-		}
-		newTargets := target.ParseLinks(content)
-		targets = append(targets, newTargets...)
-	}
+	rootTarget := &parser.Target{URL: *rootURL, Depth: 0}
+	NewScrapper(maxDepth).Run(rootTarget)
 }
